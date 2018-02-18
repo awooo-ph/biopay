8221bbbcf6c7ce216eab68eda64f611fa40427a3
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-02-19 03:01:37 +0800

    resources

--- NORSU.BioPay/App.xaml.cs
+++ NORSU.BioPay/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 
 namespace NORSU.BioPay
@@ -12,5 +13,11 @@ namespace NORSU.BioPay
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            awooo.IsRunning = true;
+            awooo.Context = SynchronizationContext.Current;
+        }
     }
 }
