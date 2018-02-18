2d916a8c3ea1bc9144aa266a2c85d495c0572121
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-02-19 03:02:41 +0800

    clock

---
+++ NORSU.BioPay/Views/Clock.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace NORSU.BioPay.Views
+{
+    /// <summary>
+    /// Interaction logic for Clock.xaml
+    /// </summary>
+    public partial class Clock : UserControl
+    {
+        public Clock()
+        {
+            InitializeComponent();
+            
+            Tick();
+            ToggleMinutes();
+        }
+
+        private async void ToggleMinutes()
+        {
+            while (true)
+            {
+                Time.DisplayMode = ClockDisplayMode.Hours;
+                await TaskEx.Delay(7777);
+                Time.DisplayMode = ClockDisplayMode.Minutes;
+                await TaskEx.Delay(4444);
+            }
+        }
+
+        private async void Tick()
+        {
+            while (true)
+            {
+                Time.Time = DateTime.Now;
+                await TaskEx.Delay(111);
+            }
+        }
+    }
+}
