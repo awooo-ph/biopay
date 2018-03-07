393e404f031dc322036d7736b9980fe01c62173d
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-03-07 22:19:43 +0800

    light theme

---
+++ NORSU.BioPay/ViewModels/Enumerations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NORSU.BioPay.ViewModels
+{
+    enum Months
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
